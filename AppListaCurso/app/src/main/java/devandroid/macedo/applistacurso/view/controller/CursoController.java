e1de06ef18d6be25af700c40fe3ca9eb7ff637dd
ygormacedo <ygormacedo44@gmail.com>
2025-01-13 19:32:19 -0300

    1

--- AppListaCurso/app/src/main/java/devandroid/macedo/applistacurso/view/controller/CursoController.java
+++
@@ -1,9 +0,0 @@
-package devandroid.macedo.applistacurso.view.controller;
-
-import devandroid.macedo.applistacurso.view.model.Pessoa;
-
-public class CursoController extends Pessoa {
-
-
-
-}
---
+++ AppListaCurso/app/src/main/java/devandroid/macedo/applistacurso/view/controller/PessoaController.java
@@ -0,0 +1,22 @@
+package devandroid.macedo.applistacurso.view.controller;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.macedo.applistacurso.view.model.Pessoa;
+
+public class PessoaController {
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+
+        Log.i("MVC_CONTROLLER","COntroller Salva: "+pessoa.toString());
+
+    }
+}
